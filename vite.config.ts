8aa57cf885b1407e8aacb66ba28f13d74a033cf6
djuarez-art <d.juarez@sileo.com.mx>
2026-08-21 02:57:42 -0600

    fix(config): correct base path syntax in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "/reto-sileo/,"
+    base: '/reto-sileo/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
